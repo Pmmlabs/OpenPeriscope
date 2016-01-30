1f81451f5e9761e0696070f3644188abdc194b2c
Pmmlabs <kilowatt36@mail.ru>
2016-01-30 23:36:21 +0300

    suggested people

--- Periscope_Web_Client.user.js
+++ Periscope_Web_Client.user.js
@@ -178,17 +178,20 @@ if (location.href.indexOf('twitter.com/oauth/404') > 0) {
         float: left;\
         margin-right: 10px;\
     }\
-    .chatlink, .watching {\
+    .chatlink, .watching, .hearts {\
         float: right;\
-        padding-left: 20px;\
+        padding-left: 40px;\
         background-repeat: no-repeat;\
-        background-position: 0 center;\
+        background-position: 20px center;\
     }\
     .chatlink {\
         background-image: url("//raw.githubusercontent.com/Pmmlabs/OpenPeriscope/master/images/comment-black.png");\
     }\
     .watching {\
         background-image: url("//raw.githubusercontent.com/Pmmlabs/OpenPeriscope/master/images/user-black.png");\
+    }\
+    .hearts {\
+        background-image: url("//raw.githubusercontent.com/Pmmlabs/OpenPeriscope/master/images/heart-black.png");\
     }\
     dt {\
         width: 150px;\
@@ -306,9 +309,8 @@ if (location.href.indexOf('twitter.com/oauth/404') > 0) {
         height: 1.5em;\
     }\
     /* USER */\
-    .avatar {\
-        float: left;\
-        margin-right: 10px;\
+    img.avatar {\
+        border: none;\
     }\
 </style>')
         .append('<link href="https://fonts.googleapis.com/css?family=Roboto&subset=latin,cyrillic" rel="stylesheet" type="text/css">');
@@ -358,6 +360,7 @@ function Ready(loginInfo) {
         {text: 'Feeds', id: 'Top'},
         {text: 'New broadcast', id: 'Create'},
         {text: 'Chat', id: 'Chat'},
+        {text: 'Suggested people', id: 'People'},
         {text: 'User', id: 'User'}
     ];
     for (var i in menu) {
@@ -380,6 +383,27 @@ function SwitchSection(elem, section) {
     else
         sectionContainer.show();
 }
+var languageSelect = '<dt>Language: <select class="lang">\
+            <option>ar</option>\
+            <option>de</option>\
+            <option>en</option>\
+            <option>es</option>\
+            <option>fi</option>\
+            <option>fr</option>\
+            <option>hy</option>\
+            <option>id</option>\
+            <option>it</option>\
+            <option>ja</option>\
+            <option>kk</option>\
+            <option>other</option>\
+            <option>pt</option>\
+            <option>ro</option>\
+            <option>ru</option>\
+            <option>sv</option>\
+            <option>tr</option>\
+            <option>uk</option>\
+            <option>zh</option>\
+        </select></dt>';
 function InitMap() {
     $(document.head).append('<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css" />')
         .append('<link rel="stylesheet" href="http://leaflet.github.io/Leaflet.markercluster/dist/MarkerCluster.css" />');
@@ -526,10 +550,10 @@ function InitTop() {
             result.empty();
             var ids =[];
             for (var i in response) {
-                var stream = $('<div class="stream ' + response[i].state + ' '+response[i].id+'">').append(getDescription(response[i]));
+                var stream = $('<div class="stream ' + response[i].state + ' '+response[i].id+'"/>').append(getDescription(response[i]));
                 var link = $('<a>Get stream link</a>');
                 link.click(getM3U.bind(null, response[i].id, stream));
-                result.append(stream.append(link).append('<br/>'));
+                result.append(stream.append(link));
                 ids.push(response[i].id);
             }
             if (response.length)
@@ -542,31 +566,11 @@ function InitTop() {
         });
     };
 
-    $('#right').append('<div id="Top"><dt>Language: <select id="lang">\
-            <option>ar</option>\
-            <option>de</option>\
-            <option>en</option>\
-            <option>es</option>\
-            <option>fi</option>\
-            <option>fr</option>\
-            <option>hy</option>\
-            <option>id</option>\
-            <option>it</option>\
-            <option>ja</option>\
-            <option>kk</option>\
-            <option>other</option>\
-            <option>pt</option>\
-            <option>ro</option>\
-            <option>ru</option>\
-            <option>sv</option>\
-            <option>tr</option>\
-            <option>uk</option>\
-            <option>zh</option>\
-        </select></dt></div>');
-    $("#lang").find(":contains("+(navigator.language || navigator.userLanguage).substr(0, 2)+")").attr("selected", "selected");
+    $('#right').append($('<div id="Top"/>').append(languageSelect));
+    $("#Top .lang").find(":contains("+(navigator.language || navigator.userLanguage).substr(0, 2)+")").attr("selected", "selected");
     var tabs = [
         {text: 'Ranked', method: 'rankedBroadcastFeed', params: {
-            languages: [$('#lang').val()]
+            languages: [$('#Top .lang').val()]
         }},
         {text: 'Following', method: 'followingBroadcastFeed'},
         {text: 'Featured', method: 'featuredBroadcastFeed'},
@@ -631,27 +635,9 @@ function InitUser() {
             user_id: $('#user_id').val().trim()
         }, function (response) {
             var user = response.user;
-            user.profile_image_urls.sort(function (a, b) {
-                return a.width * a.height - b.width * b.height;
-            });
-            result.append('<a class="avatar" href="' + user.profile_image_urls[user.profile_image_urls.length - 1].url + '" target="_blank"><img width="128" src="' + user.profile_image_urls[0].url + '"></a>' +
-                user.display_name + ' (@' + user.username + ') '
-                + (user.is_twitter_verified ? '<svg viewBox="0 0 17 17" width="1em" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-767.000000, -573.000000)"><g transform="translate(-80.000000, -57.000000)"><g transform="translate(100.000000, 77.000000)"><g transform="translate(400.000000, 401.000000)"><g><g><g transform="translate(347.000000, 152.000000)"><path d="M1.74035847,11.2810213 C1.61434984,11.617947 1.54545455,11.982746 1.54545455,12.3636364 C1.54545455,14.0706983 2.92930168,15.4545455 4.63636364,15.4545455 C5.01725401,15.4545455 5.38205302,15.3856502 5.71897873,15.2596415 C6.22025271,16.2899361 7.2772042,17 8.5,17 C9.7227958,17 10.7797473,16.2899361 11.2810213,15.2596415 L11.2810213,15.2596415 C11.617947,15.3856502 11.982746,15.4545455 12.3636364,15.4545455 C14.0706983,15.4545455 15.4545455,14.0706983 15.4545455,12.3636364 C15.4545455,11.982746 15.3856502,11.617947 15.2596415,11.2810213 C16.2899361,10.7797473 17,9.7227958 17,8.5 C17,7.2772042 16.2899361,6.22025271 15.2596415,5.71897873 C15.3856502,5.38205302 15.4545455,5.01725401 15.4545455,4.63636364 C15.4545455,2.92930168 14.0706983,1.54545455 12.3636364,1.54545455 C11.982746,1.54545455 11.617947,1.61434984 11.2810213,1.74035847 C10.7797473,0.71006389 9.7227958,0 8.5,0 C7.2772042,0 6.22025272,0.71006389 5.71897873,1.74035847 C5.38205302,1.61434984 5.01725401,1.54545455 4.63636364,1.54545455 C2.92930168,1.54545455 1.54545455,2.92930168 1.54545455,4.63636364 C1.54545455,5.01725401 1.61434984,5.38205302 1.74035847,5.71897873 C0.71006389,6.22025272 0,7.2772042 0,8.5 C0,9.7227958 0.71006389,10.7797473 1.74035847,11.2810213 L1.74035847,11.2810213 Z" class="verified-bg" opacity="1" fill="#88C9F9"></path><path d="M11.2963464,5.28945679 L6.24739023,10.2894568 L7.63289664,10.2685106 L5.68185283,8.44985845 C5.27786241,8.07328153 4.64508754,8.09550457 4.26851062,8.499495 C3.8919337,8.90348543 3.91415674,9.53626029 4.31814717,9.91283721 L6.26919097,11.7314894 C6.66180802,12.0974647 7.27332289,12.0882198 7.65469737,11.7105432 L12.7036536,6.71054321 C13.0960757,6.32192607 13.0991603,5.68876861 12.7105432,5.29634643 C12.3219261,4.90392425 11.6887686,4.90083965 11.2963464,5.28945679 L11.2963464,5.28945679 Z" class="verified-check" fill="#FFFFFF"></path></g></g></g></g></g></g></g></g></svg>' : '')
-                + '<br/>Created: ' + (new Date(user.created_at)).toLocaleString()
-                + (user.description ? '<br/>' + user.description : ''))
-                .append('<br/>')
-                .append($('<a class="button">'+(user.is_following ? 'unfollow' : 'follow')+'</a>').click(function(){
-                    var el = this;
-                    Api(el.innerHTML, { // follow or unfollow
-                        user_id: user.id
-                    }, function(r){
-                        if (r.success)
-                            el.innerHTML = el.innerHTML == 'follow' ? 'unfollow' : 'follow';
-                    })
-                }))
-                .append('<div style="clear:both"/>');
+            result.append(getUserDescription(user));
             Api('userBroadcasts', {
-                user_id: $('#user_id').val().trim(),
+                user_id: user.id,
                 all: true
             }, function(streams){
                 var ids =[];
@@ -659,7 +645,7 @@ function InitUser() {
                     var stream = $('<div class="stream ' + streams[i].state + ' '+streams[i].id+'">').append(getDescription(streams[i]));
                     var link = $('<a>Get stream link</a>');
                     link.click(getM3U.bind(null, streams[i].id, stream));
-                    result.append(stream.append(link).append('<br/>'));
+                    result.append(stream.append(link));
                     ids.push(streams[i].id);
                 }
                 Api('getBroadcasts', {
@@ -675,6 +661,32 @@ function InitUser() {
     var showButton = $('<a class="button" id="showuser">OK</a>');
     showButton.click(refreshList);
     $('#User').append(showButton).append('<br/><br/><div id="resultUser" />');
+}
+function InitPeople() {
+    var refreshButton = $('<a class="button">Refresh</a>').click(function () {
+        Api('suggestedPeople', {
+            languages: [$('#People .lang').val()]
+        }, function (response) {
+            var result = $('#resultPeople');
+            result.html('<h1>Featured</h1>');
+            for (var i in response.featured)
+                result.append($('<div class="stream"/>').append(getUserDescription(response.featured[i])));
+            result.append('<h1>Popular</h1>');
+            for (var i in response.popular)
+                result.append($('<div class="stream"/>').append(getUserDescription(response.popular[i])));
+            Api('suggestedPeople', {}, function (response) {
+                result.append('<h1>Hearted</h1>');
+                for (var i in response.hearted)
+                    result.append($('<div class="stream"/>').append(getUserDescription(response.hearted[i])));
+            });
+        });
+    });
+    $('#right').append($('<div id="People"/>')
+        .append(languageSelect)
+        .append(refreshButton)
+        .append('<div id="resultPeople" />'));
+    $("#People .lang").find(":contains(" + (navigator.language || navigator.userLanguage).substr(0, 2) + ")").attr("selected", "selected");
+    refreshButton.click();
 }
 var chat_interval;
 var presence_interval;
@@ -910,6 +922,30 @@ function getDescription(stream, lazyload) {
     chatLink.click(openChat.bind(null, stream.id));
     description.append(chatLink).append('<div class="links" />');
     return description[0];
+}
+function getUserDescription(user) {
+    user.profile_image_urls.sort(function (a, b) {
+        return a.width * a.height - b.width * b.height;
+    });
+    return $('<div class="description"/>')
+        .append('<a href="' + user.profile_image_urls[user.profile_image_urls.length - 1].url + '" target="_blank"><img class="avatar" width="128" src="' + user.profile_image_urls[0].url + '"></a>'
+        + '<div class="watching">' + user.n_followers + '</div>'
+        + (user.n_hearts ? '<div class="hearts">' + user.n_hearts + '</div>' : ''))
+        .append($('<a class="username">' + emoji.replace_unified(user.display_name) + ' (@' + user.username + ')</a>').click(openUser.bind(null, user.id)))
+        .append((user.is_twitter_verified ? '<svg viewBox="0 0 17 17" width="1em" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-767.000000, -573.000000)"><g transform="translate(-80.000000, -57.000000)"><g transform="translate(100.000000, 77.000000)"><g transform="translate(400.000000, 401.000000)"><g><g><g transform="translate(347.000000, 152.000000)"><path d="M1.74035847,11.2810213 C1.61434984,11.617947 1.54545455,11.982746 1.54545455,12.3636364 C1.54545455,14.0706983 2.92930168,15.4545455 4.63636364,15.4545455 C5.01725401,15.4545455 5.38205302,15.3856502 5.71897873,15.2596415 C6.22025271,16.2899361 7.2772042,17 8.5,17 C9.7227958,17 10.7797473,16.2899361 11.2810213,15.2596415 L11.2810213,15.2596415 C11.617947,15.3856502 11.982746,15.4545455 12.3636364,15.4545455 C14.0706983,15.4545455 15.4545455,14.0706983 15.4545455,12.3636364 C15.4545455,11.982746 15.3856502,11.617947 15.2596415,11.2810213 C16.2899361,10.7797473 17,9.7227958 17,8.5 C17,7.2772042 16.2899361,6.22025271 15.2596415,5.71897873 C15.3856502,5.38205302 15.4545455,5.01725401 15.4545455,4.63636364 C15.4545455,2.92930168 14.0706983,1.54545455 12.3636364,1.54545455 C11.982746,1.54545455 11.617947,1.61434984 11.2810213,1.74035847 C10.7797473,0.71006389 9.7227958,0 8.5,0 C7.2772042,0 6.22025272,0.71006389 5.71897873,1.74035847 C5.38205302,1.61434984 5.01725401,1.54545455 4.63636364,1.54545455 C2.92930168,1.54545455 1.54545455,2.92930168 1.54545455,4.63636364 C1.54545455,5.01725401 1.61434984,5.38205302 1.74035847,5.71897873 C0.71006389,6.22025272 0,7.2772042 0,8.5 C0,9.7227958 0.71006389,10.7797473 1.74035847,11.2810213 L1.74035847,11.2810213 Z" opacity="1" fill="#88C9F9"></path><path d="M11.2963464,5.28945679 L6.24739023,10.2894568 L7.63289664,10.2685106 L5.68185283,8.44985845 C5.27786241,8.07328153 4.64508754,8.09550457 4.26851062,8.499495 C3.8919337,8.90348543 3.91415674,9.53626029 4.31814717,9.91283721 L6.26919097,11.7314894 C6.66180802,12.0974647 7.27332289,12.0882198 7.65469737,11.7105432 L12.7036536,6.71054321 C13.0960757,6.32192607 13.0991603,5.68876861 12.7105432,5.29634643 C12.3219261,4.90392425 11.6887686,4.90083965 11.2963464,5.28945679 L11.2963464,5.28945679 Z" fill="#FFFFFF"></path></g></g></g></g></g></g></g></g></svg>' : '')
+        + '<br/>Created: ' + (new Date(user.created_at)).toLocaleString()
+        + (user.description ? '<br/>' + emoji.replace_unified(user.description) : ''))
+        .append('<br/>')
+        .append($('<a class="button">' + (user.is_following ? 'unfollow' : 'follow') + '</a>').click(function () {
+            var el = this;
+            Api(el.innerHTML, { // follow or unfollow
+                user_id: user.id
+            }, function (r) {
+                if (r.success)
+                    el.innerHTML = el.innerHTML == 'follow' ? 'unfollow' : 'follow';
+            })
+        }))
+        .append('<div style="clear:both"/>');
 }
 function openChat(broadcast_id){
     SwitchSection(null, 'Chat');
