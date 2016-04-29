71e615670e8800ddd754a435e4a6da395f6cb069
Pmmlabs <kilowatt36@mail.ru>
2016-04-29 16:13:16 +0300

    improve progressbar

--- Periscope_Web_Client.user.js
+++ Periscope_Web_Client.user.js
@@ -568,16 +568,17 @@ var Progress = {
     start: function(){
         this.count++;
         this.elem.css('visibility', 'visible');
-        this.elem.css('width','100%');
+        this.elem.css('width', Math.floor(100/this.count)+'%');
     },
     stop: function(){
         this.count--;
-        if (!this.count) {  // if there is unfinished requests
+        if (!this.count) {  // if there is no unfinished requests
             this.elem.css('width', '0');
             this.elem.css('visibility', 'hidden');
-        }
+        } else
+            this.elem.css('width', Math.floor(100/this.count)+'%');
     }
-}
+};
 var languageSelect = '<dt>Language: <select class="lang">\
             <option>ar</option>\
             <option>de</option>\
